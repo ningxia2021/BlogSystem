4131adfac987798c1bf7c427de4e54ea24fc01e0
ningxia2021 <2424917250@qq.com>
2022-05-31 11:20:54 +0800

    123

--- src/main/java/com/example/blog/entity/User.java
+++ src/main/java/com/example/blog/entity/User.java
@@ -2,9 +2,12 @@ package com.example.blog.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class User {
+public class User implements Serializable {
     private int userId;
     private String username;
     private String password;
+
 }
